d7114c1f7c0782eead20dd6a26eeb6671638dd28
hasnainqureshi1 <husnainqureshi134@gmail.com>
2025-12-30 16:58:10 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Home from "./Pages/Home";
 import { Route,  Routes } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
  import './App.css'
 import SignIn from "./Pages/SignIn";
 import MarketPlace from "./Pages/MarketPlace";
@@ -8,6 +9,7 @@ import Market from "./Pages/Market";
 
 function App() {
   return (
+    
     <div className="absolute top-0 left-0 w-full h-full overflow-hidden bg-mainColor" >
         <Routes>
         <Route path= '/' element={<Home/>} /> 
